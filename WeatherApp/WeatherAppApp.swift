045c8b729a6d9ec9ba3461e1a364993ee3df62cf
Hari-Rait <86234153+Hari-Rait@users.noreply.github.com>
2024-02-20 20:56:27 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Hari Rait on 20.02.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
